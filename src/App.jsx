9ca45765b0a75f6ea1c05fef3eeff365c9642a72
RobertDiNicolasCoke <133121993+RobertDiNicolasCoke@users.noreply.github.com>
2025-01-28 16:24:31 -0500

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DateRangePicker from './components/DateRangePicker';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: '20px 0px 0px 100px' }}>
       <DateRangePicker />
     </div>
   );
