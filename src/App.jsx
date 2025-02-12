06a49186a7e7595c1d20560e11e17b06d006d6d2
RobertDiNicolasCoke <133121993+RobertDiNicolasCoke@users.noreply.github.com>
2025-02-12 12:07:43 -0500

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,9 @@ function App() {
       <DateRangePicker 
         // Number of months to display (1-6)
         visibleMonths={2}
-
         // Show month headings
         showMonthHeadings={true}
-
-        // Optional callbacks
-        // onChange={(dates) => console.log(dates)}  // {start: Date, end: Date}
-        // onApply={(dates) => console.log(dates)}   // {start: Date, end: Date}
-        // onClear={() => console.log('cleared')}    // void
-
-        // Optional styling
-        // className="custom-class"                  // string
-        // style={{ width: '300px' }}               // React.CSSProperties
-
-        // Optional date constraints
-        // minDate={new Date()}                     // Date
-        // maxDate={new Date('2025-12-31')}         // Date
-        
-        // Optional initial dates
-        // defaultStartDate={new Date()}            // Date
-        // defaultEndDate={new Date()}              // Date
-
+        // Single or range selection
         selectionMode="single"  // or "range"
       />
     </div>
