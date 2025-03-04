860b2962ecbb96ad19cae6743e497ad29fd00cf9
RobertDiNicolasCoke <133121993+RobertDiNicolasCoke@users.noreply.github.com>
2025-03-04 15:53:40 -0500

    build/ts fixes

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: 'dist/index.js',
@@ -21,11 +22,17 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve({
-      extensions: ['.js', '.jsx']
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      sourceMap: true,
+      inlineSources: true
     }),
     babel({
       exclude: 'node_modules/**',
-      presets: ['@babel/preset-react']
+      presets: ['@babel/preset-react'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
     }),
     commonjs(),
     postcss({
